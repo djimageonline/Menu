5d48ab3de344581b460edbb3cd42d431182ba1de
Johnny Proano <jproano22@yahoo.com>
2024-09-17 15:18:17 -0400

    Updated App added Array and Strucute with Content

---
+++ Menu/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Johnny Proano on 9/17/24.
+//
+
+import Foundation
+
+struct MenuItem {
+    var name:String
+    var price:String
+    var imageName:String
+}
