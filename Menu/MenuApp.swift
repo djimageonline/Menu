6face37536a864fd55b15095746518c54200bed3
Johnny Proano <jproano22@yahoo.com>
2024-09-17 11:03:17 -0400

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Johnny Proano on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
