6b301f76fc767dfe70891779fa616af0084b3fbd
Johnny Proano <jproano22@yahoo.com>
2024-09-18 15:42:53 -0400

    Seperateed and created resuable component, file for Data Service, and Menu List to create instances.

--- Menu/MenuItem.swift
+++ Menu/MenuItem.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct MenuItem: Identifiable {
     var id: UUID =  UUID()
-    var name:String
-    var price:String
-    var imageName:String
+    var name: String
+    var price: String
+    var imageName: String
 }
